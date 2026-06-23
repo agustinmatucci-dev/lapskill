b5329b22653071d2744af78f268196df37b6ecde
agustinmatucci-dev <agustinmatucci@gmail.com>
2026-06-23 15:23:34 -0300

    fix: corregir Link y a tags en nav-logo

--- app/login/page.js
+++ app/login/page.js
@@ -76,7 +76,7 @@ export default function Login() {
             </button>
           </form>
           <div className="auth-link">
-            ¿No tenés cuenta? <a href="/registro">Registrate gratis</Link>
+            ¿No tenés cuenta? <a href="/registro">Registrate gratis</a>
           </div>
         </div>
       </div>
